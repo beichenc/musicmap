422551a85f1c4c2b0bb049d3b5323d552a6ef37c
Amy Chen <ajchen@kth.se>
2017-04-28 14:33:18 +0200

    customized marker

--- app/components/Home.js
+++ app/components/Home.js
@@ -38,8 +38,49 @@ class Home extends React.Component {
       lat: this.state.pos.lat,
       lng: this.state.pos.lng
     });
-    console.log(this.state.accountDetails.item.name);
+    var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: this.pos
+        });
+
+        setMarkers(map);
+
   }
+
+  setMarkers(map) {
+        // Adds markers to the map.
+
+        // Marker sizes are expressed as a Size of X,Y where the origin of the image
+        // (0,0) is located in the top left of the image.
+
+        // Origins, anchor positions and coordinates of the marker increase in the X
+        // direction to the right and in the Y direction down.
+        var image = {
+          url: this.state.songimg,
+          // This marker is 20 pixels wide by 32 pixels high.
+          size: new google.maps.Size(20, 32),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (0, 32).
+          anchor: new google.maps.Point(0, 32)
+        };
+        // Shapes define the clickable region of the icon. The type defines an HTML
+        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+        // The final coordinate closes the poly by connecting to the first coordinate.
+        var shape = {
+          coords: [1, 1, 1, 20, 18, 20, 18, 1],
+          type: 'poly'
+        };
+          var marker = new google.maps.Marker({
+            position: this.state.pos,
+            map: map,
+            icon: image,
+            shape: shape,
+            title: this.state.accountDetails.item.name,
+            zIndex: currentdate.getFullYear() + currentdate.getMonth()+1 + currentdate.getDate()+this.state.accountDetails.item.name
+          });
+
+      }
 
   handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
