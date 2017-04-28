1e52e3ccdeb4597063b4af4f86fd7b5b632cddc5
Beichen Chen <beichen@kth.se>
2017-04-28 14:53:15 +0200

    add image to marker

--- app/components/Home.js
+++ app/components/Home.js
@@ -23,7 +23,43 @@ class Home extends React.Component {
       isLoggedIn: false,
       pos: {}
     };
+    this.map = {}
   }
+
+  setMarkers(map) {
+    // Adds markers to the map.
+
+    // Marker sizes are expressed as a Size of X,Y where the origin of the image
+    // (0,0) is located in the top left of the image.
+
+    // Origins, anchor positions and coordinates of the marker increase in the X
+    // direction to the right and in the Y direction down.
+    var image = {
+      url: this.state.songimg,
+      // This marker is 20 pixels wide by 32 pixels high.
+      size: new google.maps.Size(64, 64),
+      // The origin for this image is (0, 0).
+      origin: new google.maps.Point(0, 0),
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new google.maps.Point(0, 32)
+    };
+    // Shapes define the clickable region of the icon. The type defines an HTML
+    // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+    // The final coordinate closes the poly by connecting to the first coordinate.
+    var shape = {
+      coords: [1, 1, 1, 20, 18, 20, 18, 1],
+      type: 'poly'
+    };
+    var marker = new google.maps.Marker({
+      position: this.state.pos,
+      map: map,
+      icon: image,
+      shape: shape,
+      title: this.state.accountDetails.item.name
+    });
+
+  }
+
   mapSong() {
     var currentdate = new Date();
     firebase.database().ref('marker/').push({
@@ -38,49 +74,10 @@ class Home extends React.Component {
       lat: this.state.pos.lat,
       lng: this.state.pos.lng
     });
-    var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: this.pos
-        });
 
-        setMarkers(map);
+    this.setMarkers(this.map);
 
   }
-
-  setMarkers(map) {
-        // Adds markers to the map.
-
-        // Marker sizes are expressed as a Size of X,Y where the origin of the image
-        // (0,0) is located in the top left of the image.
-
-        // Origins, anchor positions and coordinates of the marker increase in the X
-        // direction to the right and in the Y direction down.
-        var image = {
-          url: this.state.songimg,
-          // This marker is 20 pixels wide by 32 pixels high.
-          size: new google.maps.Size(20, 32),
-          // The origin for this image is (0, 0).
-          origin: new google.maps.Point(0, 0),
-          // The anchor for this image is the base of the flagpole at (0, 32).
-          anchor: new google.maps.Point(0, 32)
-        };
-        // Shapes define the clickable region of the icon. The type defines an HTML
-        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-        // The final coordinate closes the poly by connecting to the first coordinate.
-        var shape = {
-          coords: [1, 1, 1, 20, 18, 20, 18, 1],
-          type: 'poly'
-        };
-          var marker = new google.maps.Marker({
-            position: this.state.pos,
-            map: map,
-            icon: image,
-            shape: shape,
-            title: this.state.accountDetails.item.name,
-            zIndex: currentdate.getFullYear() + currentdate.getMonth()+1 + currentdate.getDate()+this.state.accountDetails.item.name
-          });
-
-      }
 
   handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
@@ -93,7 +90,7 @@ class Home extends React.Component {
   componentDidMount() {
     var infoWindow = new google.maps.InfoWindow;
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    this.map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: {lat: -34.397, lng: 150.644}
     });
@@ -106,7 +103,7 @@ class Home extends React.Component {
         };
         var marker = new google.maps.Marker({
           position: pos,
-          map: map
+          map: this.map
         });
         this.setState(function() {
           return {
@@ -117,14 +114,14 @@ class Home extends React.Component {
           content: "<p style='color: black;'> Location found. </p>"
         })
         infoWindow.setPosition(pos);
-        infoWindow.open(map);
-        map.setCenter(pos);
+        infoWindow.open(this.map);
+        this.map.setCenter(pos);
       }.bind(this), function() {
-        handleLocationError(true, infoWindow, map.getCenter());
+        handleLocationError(true, infoWindow, this.map.getCenter());
       });
     } else {
       // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+      handleLocationError(false, infoWindow, this.map.getCenter());
     }
 
     var menu = require('../js/menu.js');
